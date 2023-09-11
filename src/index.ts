d8130006f7493c0adc7644df7eea164c0cd5e443
Viraj Shah <veer15102003@gmail.com>
2023-09-11 13:08:31 +0530

    feat: added all db paths

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,56 @@
-console.log("Hello via Bun!");
+import { Elysia, t } from "elysia";
+import { html } from "@elysiajs/html";
+import { NotesDb } from "../db";
+
+new Elysia()
+  .use(html())
+  .decorate("db", new NotesDb())
+  .get("/", () => Bun.file("index.html").text)
+  .get("/script.js", () => Bun.file("script.js").text())
+  .get("/notes", ({ db }) => db.getAllNotes())
+  .post(
+    async ({ db, body }) => {
+      console.log(body);
+      const id = (await db.addNote(body)).id;
+      console.log(id);
+      return { success: true, id };
+    },
+    {
+      schema: {
+        body: t.Object({
+          title: t.String(),
+          content: t.String(),
+        }),
+      },
+    },
+  )
+  .put(
+    "/notes/:id",
+    ({ db, params, body }) => {
+      try {
+        db.changeTitle(parseInt(params.id), body);
+        return { success: true };
+      } catch (e) {
+        return { success: false };
+      }
+    },
+    {
+      schema: {
+        body: t.Object({
+          title: t.String(),
+          content: t.String(),
+        }),
+      },
+    },
+  )
+
+  .delete("/notes/:id", ({ db, params }) => {
+    try {
+      db.removeNote(parseInt(params.id));
+      return { success: true };
+    } catch (e) {
+      return { success: false };
+    }
+  })
+
+  .listen(8000);
